1b804882805d0e9f5a3587df287bb30c7e5b057e
VickyMVGL <gonzalezma22@ujap.edu.ve>
2025-04-20 10:17:19 -0400

    Empezando la conexion con la Api

---
+++ proyecto_recetario/lib/screens/maintenance_detail.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+
+
+class MaintenanceDetail extends StatelessWidget {
+  final String maintenanceName;
+  const MaintenanceDetail({super.key, required this.maintenanceName});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(maintenanceName, style: TextStyle(color: Colors.white)),
+        backgroundColor: Colors.deepOrange,
+        leading: IconButton(onPressed:() {Navigator.pop(context);}, 
+        icon: Icon(Icons.arrow_back_ios_new), 
+        color: Colors.white,),
+      ),
+    );
+  }
+}
